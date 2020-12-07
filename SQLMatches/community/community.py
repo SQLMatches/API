07e7f58cda284ee895fc2643a00aae5aa2d4594d
Ward <wardpearce@protonmail.com>
2020-12-07 15:37:27 +1300

    Update community.py

--- SQLMatches/community/community.py
+++ SQLMatches/community/community.py
@@ -20,6 +20,7 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 """
 
+
 from typing import AsyncGenerator, List
 from uuid import uuid4
 from datetime import datetime
