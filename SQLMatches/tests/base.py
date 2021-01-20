df3049a2a1df5b52ce122629f1170be879b10bc5
Ward <wardpearce@protonmail.com>
2021-01-21 09:46:40 +1300

    Updated copyright header

--- SQLMatches/tests/base.py
+++ SQLMatches/tests/base.py
@@ -2,7 +2,7 @@
 
 """
 GNU General Public License v3.0 (GPL v3)
-Copyright (c) 2020-2020 WardPearce
+Copyright (c) 2020-2021 WardPearce
 Permission is hereby granted, free of charge, to any person obtaining a
 copy of this software and associated documentation files (the "Software"),
 to deal in the Software without restriction, including without limitation
