b413fdf57f0c21c642f3e640175524a2b4a38703
WardPearce <wardpearce@protonmail.com>
2022-02-07 20:01:07 +1300

    Fixed import for settings

--- SQLMatches/settings/__init__.py
+++ SQLMatches/settings/__init__.py
@@ -1,11 +1,9 @@
 from .demo import DemoSettings
 from .database import DatabaseSettings
-from .match import MatchSettings
 from .steam import SteamSettings
 
 __all__ = [
     "DemoSettings",
     "DatabaseSettings",
-    "MatchSettings",
     "SteamSettings"
 ]
